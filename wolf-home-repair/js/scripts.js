5f9d04440020427a0bdde6fce8de0313017f6c62
Chris Wolf <chriswolf@Chriss-MacBook-Pro-2.local>
2018-09-11 10:12:54 -0400

    "Completed: Wolf Home Repair -- slideshow

---
+++ wolf-home-repair/js/scripts.js
@@ -0,0 +1,19 @@
+const TOTAL_IMAGES = 8;
+
+$("document").ready(function() {
+	let slide = 1;
+
+	// TODO: Continue working on slideshow
+	setInterval(function() {
+		slide++;
+		if(slide === 9) {
+			slide = 1;
+		}
+
+		// BUG: Smooth out fadeout and fadein
+		// $("#slideshow-image").fadeOut(1000);
+		$("#slideshow-image").attr("src", "images/image" + slide + ".jpg");
+		// $("#slideshow-image").fadeIn(1000);
+
+	}, 3000);
+});
