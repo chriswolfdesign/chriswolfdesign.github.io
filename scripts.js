dff81fb274520408f2dda1d0bdf22833d8b58c2b
chriswolfdesign <chriswolfdesign@gmail.com>
2019-02-27 05:17:49 -0500

    Moving to master

--- scripts.js
+++ scripts.js
@@ -1,12 +1,8 @@
 // Set up original values and settings
 $(document).ready(function() {
-    $("#technology-sidebar").hide();
-
     // If the view technology button is clicked, have the technology sidebar
     // either slide into or out of view as appropriate
     $("#technology").click(function() {
-        console.log("I was clicked.");
         $("#technology-sidebar").animate({width: 'toggle'}, 90);
-        // $("#technology-sidebar").show("slide", {direction: "left"}, 100000);
     });
 });
