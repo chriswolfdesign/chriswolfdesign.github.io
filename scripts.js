87f7b17eddb39c00050ea788cc6a27065e25299e
chriswolfdesign <chriswolfdesign@gmail.com>
2019-03-19 13:30:42 -0400

    Added documentation

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
+/**
+ * scripts.js
+ *
+ * Determines the behavior of index.html
+ *
+ * @author Chris Wolf
+ * @version 1.0.0 (March 19, 2019)
+ * @contact chriswolfdesign@gmail.com
+ */
+
 // Set up original values and settings
 $(document).ready(function() {
     // If the view technology button is clicked, have the technology sidebar
